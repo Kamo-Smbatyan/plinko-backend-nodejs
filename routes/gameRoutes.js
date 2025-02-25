e6ceb5f06c3c9f04d18316cccf860f7e79e35d77
Candy Boy <octorcat@gmail.com>
2025-02-25 08:52:19 -0800

    test update

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -5,7 +5,6 @@ const {createWebhookAdmin} = require('../config/webhook');
 
 router.get("/events", async (req, res) => {
     const {telegramID} = req.query;
-
     res.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
@@ -18,8 +17,15 @@ router.get("/events", async (req, res) => {
     });
 });
 
-router.get('/test', async (req, res) => {
-    const result = await createWebhookAdmin();
+router.get('/testSSE', async (req, res) => {
+    let result;
+    if (clients.length > 0){
+        clients[0].write('data: ' + 'Success' + '\n\n');
+        result = true
+    }
+    else(
+        result= false
+    )
     return res.json(result);
 })
 
